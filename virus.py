c76168490824810c6d805ee44a96c46d31e0963d
insta-nature <85687680+insta-nature@users.noreply.github.com>
2021-06-11 12:49:57 +0530

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,2 +1,23 @@
-import turtle as tl
-print(dir(tl))
+import turtle
+obj = turtle.Turtle()
+obj.speed(0)
+obj.pencolor("Red")
+#obj.bgcolor
+
+win = turtle.screensize()
+turtle.bgcolor("black")
+turtle.title("AjeetK")
+f = 0
+r = 0
+obj.up()
+obj.goto(0,300)
+obj.down()
+while(1):
+    obj.forward(f)
+    obj.right(r)
+    f+=3
+    r+=1
+    if r >= 200:
+        break
+#obj.hideturtle()
+turtle.done()
